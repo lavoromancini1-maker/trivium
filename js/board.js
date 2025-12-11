fb6801c7ee54785706fdd53f20cb8c378258791d
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:42:01 +0100

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1,1 +1,12 @@
+// board.js
+
+export const BOARD = [
+  // Ogni casella sarà un oggetto del tipo:
+  // {
+  //   id: 0,
+  //   type: "category" | "key" | "event" | "minigame" | "scrigno" | "stradina",
+  //   category: "geografia" | "storia" | ... | null,
+  //   neighbors: [id1, id2, id3?]  // max 3 quando c'è stradina
+  // }
+];
 
