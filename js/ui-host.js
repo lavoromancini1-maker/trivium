ce2ddc742f4fecb631dcc06ed85c00faeeb5d228
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:43:14 +0100

    Update ui-host.js

--- js/ui-host.js
+++ js/ui-host.js
@@ -1,1 +1,12 @@
+import "./firebase-config.js";
+import * as FirebaseGame from "./firebase-game.js";
+import * as Board from "./board.js";
+import * as State from "./state-machine.js";
+import * as UIHost from "./ui-host.js";
+
+async function init() {
+  // TODO: crea partita, mostra codice, ascolta game state ecc.
+}
+
+init();
 
