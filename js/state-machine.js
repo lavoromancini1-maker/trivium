5020fec0bdcc0773d723841560d811316750c349
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:42:32 +0100

    Update state-machine.js

--- js/state-machine.js
+++ js/state-machine.js
@@ -1,1 +1,18 @@
+export const GameStates = {
+  LOBBY: "LOBBY",
+  WAIT_ROLL: "WAIT_ROLL",
+  MOVE_ANIMATION: "MOVE_ANIMATION",
+  RESOLVE_TILE: "RESOLVE_TILE",
+  QUESTION: "QUESTION",
+  MINI_GAME: "MINI_GAME",
+  DUEL: "DUEL",
+  SCRIGNO: "SCRIGNO",
+  ACCESSO_SCRIGNO: "ACCESSO_SCRIGNO",
+  // ...
+};
+
+// Funzioni tipo:
+// advanceState(game, action)
+// handlePlayerAnswer(...)
+// handleDiceRoll(...)
 
