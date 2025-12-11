6b92b56b96d2cf48cc0bccb4405c9884b2257eb1
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:41:39 +0100

    Update firebase-game.js

--- js/firebase-game.js
+++ js/firebase-game.js
@@ -1,1 +1,10 @@
+import { firebaseConfig } from "./firebase-config.js";
+// TODO: import di initializeApp, getDatabase, ref, onValue, set, update, ecc.
 
+export function createNewGame() {
+  // TODO
+}
+
+export function joinGame(gameCode, playerName) {
+  // TODO
+}
