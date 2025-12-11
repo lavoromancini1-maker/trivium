83377225a93339ab4eabe0841874e102b85699d2
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:43:39 +0100

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,1 +1,10 @@
+import "./firebase-config.js";
+import * as FirebaseGame from "./firebase-game.js";
+import * as UIPlayer from "./ui-player.js";
+
+async function init() {
+  // TODO: flusso di join game, ecc.
+}
+
+init();
 
