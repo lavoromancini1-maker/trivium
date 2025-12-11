1727b7c8902d5ad3c5ea100b86d3bab62e361cbe
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 00:40:33 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,9 @@
-
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4TXORcOqylbMKnn0ZF1nF6rjw6uo8dX4",
+    authDomain: "trivium-26048.firebaseapp.com",
+    databaseURL: "https://trivium-26048-default-rtdb.europe-west1.firebasedatabase.app"
+    projectId: "trivium-26048",
+    storageBucket: "trivium-26048.firebasestorage.app",
+    messagingSenderId: "882582165118",
+    appId: "1:882582165118:web:1dba969eda431772feea2f"
+  };
