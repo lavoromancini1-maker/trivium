5cfce9c0fc188cbfb01094f9ed98fd9b6601a551
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-12 01:05:17 +0100

    Update host.js

--- js/host.js
+++ js/host.js
@@ -1,1 +1,14 @@
+import { renderBoard } from "./ui-host.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const boardContainer = document.getElementById("board-container");
+
+  if (!boardContainer) {
+    console.error("Elemento #board-container non trovato in host.html");
+    return;
+  }
+
+  renderBoard(boardContainer);
+
+});
 
