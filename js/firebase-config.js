bfceb1184abd78c3261e3cab629245e79bc199d4
lavoromancini1-maker <lavoromancini1@gmail.com>
2025-12-14 23:26:07 +0100

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,41 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  update,
-  push,
-  onValue,
-  serverTimestamp,
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyD4TXORcOqylbMKnn0ZF1nF6rjw6uo8dX4",
-    authDomain: "trivium-26048.firebaseapp.com",
-    databaseURL: "https://trivium-26048-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "trivium-26048",
-    storageBucket: "trivium-26048.firebasestorage.app",
-    messagingSenderId: "882582165118",
-    appId: "1:882582165118:web:1dba969eda431772feea2f"
-  };
-
-
-if (!firebaseConfig || !firebaseConfig.apiKey) {
-  console.warn("⚠️ Attenzione: firebaseConfig non è impostato correttamente.");
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyD4TXORcOqylbMKnn0ZF1nF6rjw6uo8dX4",
+  authDomain: "trivium-26048.firebaseapp.com",
+  databaseURL: "https://trivium-26048-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "trivium-26048",
+  storageBucket: "trivium-26048.firebasestorage.app",
+  messagingSenderId: "882582165118",
+  appId: "1:882582165118:web:1dba969eda431772feea2f",
+};
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Esportiamo ciò che ci serve
-export {
-  db,
-  ref,
-  set,
-  get,
-  update,
-  push,
-  onValue,
-  serverTimestamp,
-};
+export const db = getDatabase(app);
